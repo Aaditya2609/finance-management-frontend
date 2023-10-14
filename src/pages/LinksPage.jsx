370aed82c6cbad8fbf058ccd26778e719d0c236e
Aaditya Khantal <khantalak2609@gmail.com>
2023-10-14 13:18:48 +0530

    update LinksPage

--- src/pages/LinksPage.jsx
+++ src/pages/LinksPage.jsx
@@ -7,7 +7,7 @@ function LinksPage() {
             <h1 className='text-3xl my-4 font-bold'>Project Links</h1>
             <p><a target="_blank" rel="noreferrer" href='https://github.com/Aaditya2609/finance-management-frontend' class="text-white bg-[#00CED1] hover:text-[#00CED1] hover:bg-[white] hover:border-2 hover:border-[#00CED1] first-letter:focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Frontend Github</a></p>
             <p><a  target="_blank" rel="noreferrer" href='https://github.com/Aaditya2609/finance-management-backend' class="text-white bg-[#00CED1] hover:text-[#00CED1]  hover:bg-[white] focus:ring-4 hover:border-2 hover:border-[#00CED1] focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Backend Github</a></p>
-            <p><a target="_blank"  rel="noreferrer" href='https://finance-management-lvbb.onrender.com' class="text-white bg-[#00CED1] hover:bg-[white] hover:text-[#00CED1] hover:border-2 hover:border-[#00CED1] first-letter:focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Backend Live Link (https://assignmen-17.cyclic.cloud)</a></p>
+            <p><a target="_blank"  rel="noreferrer" href='https://finance-management-lvbb.onrender.com' class="text-white bg-[#00CED1] hover:bg-[white] hover:text-[#00CED1] hover:border-2 hover:border-[#00CED1] first-letter:focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Backend Live Link (https://finance-management-lvbb.onrender.com)</a></p>
         </div>
     </div>
   )
